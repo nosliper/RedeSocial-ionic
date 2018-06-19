413fd27256b3c00d5a92fdbf42772a6053294fe0
nosliper <fdiego.avila@gmail.com>
2018-06-18 23:45:55 -0300

    added names in posts

--- src/pages/timeline/timeline.ts
+++ src/pages/timeline/timeline.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { PostProvider } from '../../providers/post/post';
 import { UserProvider } from '../../providers/user/user';
@@ -12,32 +12,32 @@ import { LoginPage } from '../login/login';
 export class TimelinePage {
 
     isPosting: boolean = false;
-    posts: any[] = [];
+    @Input() posts: any[] = [];
 
     constructor(public navCtrl: NavController,
         public navParams: NavParams,
         public postProvider: PostProvider,
         public userProvider: UserProvider) {
             this.fillPosts(this.posts);
-            this.fillAuthors(this.posts);
-            // this.posts.push({author: "fulano", texto: "this is the content of this post", likes: 11});
-    }
-
-    ionViewDidLoad() {
-        console.log('ionViewDidLoad TimelinePage');
+        }
+        
+        ionViewDidLoad() {
+            console.log('ionViewDidLoad TimelinePage');
     }
 
     fillPosts(posts): void {
+        let provider = this.postProvider
+        let fillAuthor = this.fillAuthor
         this.postProvider.getPosts()
         .subscribe(function(data) {
                 for(let p of data) {
-                    posts.unshift(
-                        {
-                            author: p._id,
-                            texto: p.texto,
-                            likes: p.likes
-                        }
-                    );
+                    let post = {
+                        _id: p._id,
+                        texto: p.texto,
+                        likes: p.likes
+                    }
+                    fillAuthor(post, provider);
+                    posts.unshift(post);
                 }
             },
             function(error) {
@@ -45,17 +45,14 @@ export class TimelinePage {
         })
     }
 
-    fillAuthors(posts): void {
-        for(let i in posts) {
-            this.postProvider.getPostAuthor(posts[i].author)
-            .subscribe(function(data) {
-                    console.log(data);
-                    posts[i].author = data.nome;
-                },
-                function(error) {
-                    console.log(error);
-            })
-        }
+    fillAuthor(post, provider): void {
+        provider.getPostAuthor(post._id)
+        .subscribe(function(data) {
+                post.author = data.nome;
+            },
+            function(error) {
+                console.log(error);
+        })
     }
 
     post(text: string): void {
